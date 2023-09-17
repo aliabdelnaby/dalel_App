9a39c4c0eb64ecf5eba9e262ac5302674ad1e6ba
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-09-17 23:05:01 +0300

    create App Router & splash view

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,10 @@
+import 'package:dalil/features/splash/presentation/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    builder: ((context, state) => const SplashView()),
+  ),
+  
+]);
