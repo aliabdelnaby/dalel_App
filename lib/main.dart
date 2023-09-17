365bf6b3e00e0d768128d73e08f59438a94665ef
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-09-17 23:08:46 +0300

    create App Router & App string

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dalil/core/routes/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +11,9 @@ class Dalel extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      routerConfig: router,
     );
   }
 }
